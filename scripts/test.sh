e0e45d830e36fdf5e14a46e511d5c7a493202a18
sarna <P.Sarna@f5.com>
2024-08-19 16:21:40 -0700

    Capture code coverage
    go test does not have a good way to capture
    unit-test coverage as part of test runs. This
    commit captures the error code of the unit test
    run, runs the coverage generation and then exits
    based on the test status.

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,7 +10,12 @@ fi
 outfile="${RESULTS_DIR}/coverage.out"
 mkdir -p "$RESULTS_DIR"
 go_flags+=("-cover" -coverprofile="$outfile")
+
+set +e
 gotestsum --junitfile "${RESULTS_DIR}/report.xml" --format pkgname -- "${go_flags[@]}" ./...
+rc=$?
+set -e
 echo "Total code coverage:"
 go tool cover -func="$outfile" | grep 'total:' | tee "${RESULTS_DIR}/anybadge.out"
 go tool cover -html="$outfile" -o "${RESULTS_DIR}/coverage.html"
+exit $rc
