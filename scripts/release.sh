608f0081b4df04f3c3ff9a244b4e2adf320a50f6
sarna <P.Sarna@f5.com>
2024-08-26 12:57:00 -0700

    Update release script to handle dual artifacts
    We should keep a single release script that will
    publish docker images and helm charts for the
    official release. This commit just updates the
    current release script to handle both artifact
    types. Helm logic will follow.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,46 +2,73 @@
 
 set -eo pipefail
 
-if [[ "${CI}" != "true" ]]; then
-    echo "This script is meant to be run in the CI."
-    exit 1
-fi
+docker-image() {
+    DOCKERHUB_USERNAME=$(devops.secret.get "kic-dockerhub-creds" | jq -r ".username")
+    if [[ -z "${DOCKERHUB_USERNAME}" ]]; then
+        echo "DOCKERHUB_USERNAME needs to be set."
+        exit 1
+    fi
 
-pttn="^release-[0-9]+\.[0-9]+\.[0-9]+"
-if ! [[ "${CI_COMMIT_TAG}" =~ $pttn ]]; then
-    echo "CI_COMMIT_TAG needs to be set to valid semver format."
-    exit 1
-fi
+    DOCKERHUB_PASSWORD=$(devops.secret.get "kic-dockerhub-creds" | jq -r ".password")
+    if [[ -z "${DOCKERHUB_PASSWORD}" ]]; then
+        echo "DOCKERHUB_PASSWORD needs to be set."
+        exit 1
+    fi
 
-ROOT_DIR=$(git rev-parse --show-toplevel)
-source ${ROOT_DIR}/.devops.sh
+    SRC_REGISTRY="${DEVOPS_DOCKER_URL}"
+    SRC_PATH="nginx-azure-lb/nginxaas-operator/nginxaas-operator"
+    SRC_TAG=$(echo "${CI_COMMIT_TAG}" | cut -f 2 -d "-")
+    SRC_IMG="${SRC_REGISTRY}/${SRC_PATH}:${SRC_TAG}"
 
-DOCKERHUB_USERNAME=$(devops.secret.get "kic-dockerhub-creds" | jq -r ".username")
-if [[ -z "${DOCKERHUB_USERNAME}" ]]; then
-    echo "DOCKERHUB_USERNAME needs to be set."
-    exit 1
-fi
+    DST_REGISTRY="docker.io"
+    DST_PATH="nginx/nginxaas-operator"
+    DST_TAG="${CI_COMMIT_TAG}"
+    DST_IMG="${DST_REGISTRY}/${DST_PATH}:${DST_TAG}"
 
-DOCKERHUB_PASSWORD=$(devops.secret.get "kic-dockerhub-creds" | jq -r ".password")
-if [[ -z "${DOCKERHUB_PASSWORD}" ]]; then
-    echo "DOCKERHUB_PASSWORD needs to be set."
-    exit 1
-fi
+    devops.docker.login
+    docker pull "${SRC_IMG}"
+    docker tag "${SRC_IMG}" "${DST_IMG}"
 
-SRC_REGISTRY="${DEVOPS_DOCKER_URL}"
-SRC_PATH="nginx-azure-lb/nginxaas-operator/nginxaas-operator"
-SRC_TAG=$(echo "${CI_COMMIT_TAG}" | cut -f 2 -d "-")
-SRC_IMG="${SRC_REGISTRY}/${SRC_PATH}:${SRC_TAG}"
+    # Login to Dockerhub and push release image to it.
+    docker login --username "${DOCKERHUB_USERNAME}" --password "${DOCKERHUB_PASSWORD}" "${DST_REGISTRY}"
+    docker push "${DST_IMG}"
+}
 
-DST_REGISTRY="docker.io"
-DST_PATH="nginx/nginxaas-operator"
-DST_TAG="${CI_COMMIT_TAG}"
-DST_IMG="${DST_REGISTRY}/${DST_PATH}:${DST_TAG}"
 
-devops.docker.login
-docker pull "${SRC_IMG}"
-docker tag "${SRC_IMG}" "${DST_IMG}"
+help_text() {
+    echo "Usage: $(basename $0) <artifact-type>"
+}
 
-# Login to Dockerhub and push release image to it.
-docker login --username "${DOCKERHUB_USERNAME}" --password "${DOCKERHUB_PASSWORD}" "${DST_REGISTRY}"
-docker push "${DST_IMG}"
+parse_args() {
+    if [[ "$#" -ne 1 ]]; then
+        help_text
+        exit 0
+    fi
+
+    artifact="${1}"
+    valid_artifact="(docker-image|helm-chart)"
+    valid_artifact_pttn="^${valid_artifact}$"
+    if ! [[ "${artifact}" =~ $valid_artifact_pttn ]]; then
+        echo "Invalid artifact type. Valid artifact types: $valid_artifact"
+        help_text
+        exit 1
+    fi
+}
+
+main() {
+    if [[ "${CI}" != "true" ]]; then
+        echo "This script is meant to be run in the CI."
+        exit 1
+    fi
+    pttn="^release-[0-9]+\.[0-9]+\.[0-9]+"
+    if ! [[ "${CI_COMMIT_TAG}" =~ $pttn ]]; then
+        echo "CI_COMMIT_TAG needs to be set to valid semver format."
+        exit 1
+    fi
+    parse_args "$@"
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    source ${ROOT_DIR}/.devops.sh
+    "$artifact"
+}
+
+main "$@"
