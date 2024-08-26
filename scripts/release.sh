71b9abeb6f68ffcccff785bc6c2c2797e34de252
sarna <P.Sarna@f5.com>
2024-08-26 16:28:36 -0700

    Add helm chart publishing for a release
    This commit adds logic to publish helm charts to
    dockerhub using it as an OCI helm registry.
    
    Moved around common docker code into its own
    function to reuse logic across helm and docker
    publishing.
    
    Not worrying about chart versioning right now and
    just setting up the CI for publishing.

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,36 @@
 set -eo pipefail
 
 docker-image() {
+    SRC_PATH="nginx-azure-lb/nginxaas-operator/nginxaas-operator"
+    SRC_TAG=$(echo "${CI_COMMIT_TAG}" | cut -f 2 -d "-")
+    SRC_IMG="${SRC_REGISTRY}/${SRC_PATH}:${SRC_TAG}"
+    DST_PATH="nginx/nginxaas-operator"
+    DST_TAG="${CI_COMMIT_TAG}"
+    DST_IMG="${DST_REGISTRY}/${DST_PATH}:${DST_TAG}"
+
+    docker pull "${SRC_IMG}"
+    docker tag "${SRC_IMG}" "${DST_IMG}"
+    docker push "${DST_IMG}"
+}
+
+helm-chart() {
+    SRC_PATH="nginxazurelb/charts/nginx-loadbalancer-kubernetes"
+    SRC_TAG="0.1.0"
+    SRC_CHART="oci://${SRC_REGISTRY}/${SRC_PATH}:${SRC_TAG}"
+    DST_PATH="nginxinc/charts"
+    DST_TAG="0.1.0"
+    DST_CHART="oci://${DST_REGISTRY}/${DST_PATH}"
+
+    helm pull "${SRC_CHART}" --version "${SRC_TAG}"
+    helm push nginx-loadbalancer-kubernetes-${DST_TAG}.tgz "${DST_CHART}"
+}
+
+
+help_text() {
+    echo "Usage: $(basename $0) <artifact-type>"
+}
+
+set_docker_common() {
     DOCKERHUB_USERNAME=$(devops.secret.get "kic-dockerhub-creds" | jq -r ".username")
     if [[ -z "${DOCKERHUB_USERNAME}" ]]; then
         echo "DOCKERHUB_USERNAME needs to be set."
@@ -14,29 +44,13 @@ docker-image() {
         echo "DOCKERHUB_PASSWORD needs to be set."
         exit 1
     fi
-
     SRC_REGISTRY="${DEVOPS_DOCKER_URL}"
-    SRC_PATH="nginx-azure-lb/nginxaas-operator/nginxaas-operator"
-    SRC_TAG=$(echo "${CI_COMMIT_TAG}" | cut -f 2 -d "-")
-    SRC_IMG="${SRC_REGISTRY}/${SRC_PATH}:${SRC_TAG}"
-
     DST_REGISTRY="docker.io"
-    DST_PATH="nginx/nginxaas-operator"
-    DST_TAG="${CI_COMMIT_TAG}"
-    DST_IMG="${DST_REGISTRY}/${DST_PATH}:${DST_TAG}"
 
+    # Login to NGINX DevOps Registry.
     devops.docker.login
-    docker pull "${SRC_IMG}"
-    docker tag "${SRC_IMG}" "${DST_IMG}"
-
-    # Login to Dockerhub and push release image to it.
+    # Login to Dockerhub.
     docker login --username "${DOCKERHUB_USERNAME}" --password "${DOCKERHUB_PASSWORD}" "${DST_REGISTRY}"
-    docker push "${DST_IMG}"
-}
-
-
-help_text() {
-    echo "Usage: $(basename $0) <artifact-type>"
 }
 
 parse_args() {
@@ -68,6 +82,7 @@ main() {
     parse_args "$@"
     ROOT_DIR=$(git rev-parse --show-toplevel)
     source ${ROOT_DIR}/.devops.sh
+    set_docker_common
     "$artifact"
 }
 
